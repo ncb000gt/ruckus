ef96acb737e3cb12094b86835226c4afd27a7684
Nick Campbell <nicholas.j.campbell@gmail.com>
2012-05-25 08:05:17 -0400

    Preparation for testing.
    Need some machinery to help with testing. We will need to create a
    server before each test to ensure that we have something to "cause a
    ruckus" on.
    
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

---
+++ test/utils.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+    scan = require('portscan');
+
+module.exports.server = function(cb) {
+  scan(9000, {
+    success: function(host, port) {
+      var server = http.createServer(function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World\n');
+      });
+      server.listen(port);
+    },
+    findOne: true,
+    findActive: false
+  });
+}
